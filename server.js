8082af128fd84aeb180c3fb6d2fe3f85c5669a80
xsaul <saulglz0534@gmail.com>
2025-06-04 17:00:00 -0600

    test to deploy live

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import mysql from "mysql2";
 import cors from "cors";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
-import Sequelize from "sequelize";
 
 
 dotenv.config();
@@ -14,40 +13,25 @@ app.use(express.json());
 const db = mysql.createConnection(process.env.DATABASE_URL);
 
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "mysql",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   }
-// });
 
-// sequelize.authenticate()
-//   .then(() => console.log("Connected to Railway MySQL!"))
-//   .catch(err => console.error("Database connection failed:", err));
 
 const checkBlockedUser = async (req, res, next) => {
   const { userId } = req.body;
   if (!userId) {
     return res.status(400).json({ message: "User ID missing in request." });
   }
-  // db.query("SELECT isBlocked FROM users WHERE id = ?", [userId], (err, result) => {
-  //   if (err) return res.status(500).json({ error: err.message });
-  //   if (result.length === 0) {
-  //     console.error("User ID not found in database:", userId);
-  //     return res.status(404).json({ message: "User not found." });
-  //   }
-  //   if (result[0].isBlocked) {
-  //     console.error("User is blocked:", userId);
-  //     return res.status(403).json({ message: "Your account is blocked. Redirecting to login." });
-  //   }
-  //   next();
-  // });
-  const user = await User.findOne({ where: { id: userId } });
-if (!user) return res.status(404).json({ message: "User not found." });
-if (user.isBlocked) return res.status(403).json({ message: "Your account is blocked." });
+  db.query("SELECT isBlocked FROM users WHERE id = ?", [userId], (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (result.length === 0) {
+      console.error("User ID not found in database:", userId);
+      return res.status(404).json({ message: "User not found." });
+    }
+    if (result[0].isBlocked) {
+      console.error("User is blocked:", userId);
+      return res.status(403).json({ message: "Your account is blocked. Redirecting to login." });
+    }
+    next();
+  });
 };
 
 app.post("/signup", async (req, res) => {
@@ -56,38 +40,31 @@ app.post("/signup", async (req, res) => {
     return res.status(400).json({ message: "Email and Password required" });
   }
   const hashedPassword = await bcrypt.hash(password, 10);
-  // db.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
-  //   [name, email, hashedPassword],
-  //   (err) => {
-  //     if (err) return res.status(500).json({ error: err.message });
-  //     res.json({ message: "User registered successfully!" });
-  //   }
-  // );
-  await User.create({ name, email, password: hashedPassword });
-res.json({ message: "User registered successfully!" });
-
+  db.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+    [name, email, hashedPassword],
+    (err) => {
+      if (err) return res.status(500).json({ error: err.message });
+      res.json({ message: "User registered successfully!" });
+    }
+  );
 });
 
 app.get("/users", async (req, res) => {
-  // db.query("SELECT id, name, email, last_seen, isBlocked FROM users", (err, results) => {
-  //   if (err) {
-  //     return res.status(500).json({ error: "Database error" });
-  //   }
-  //   const formattedUsers = results.map(user => {
-  //     const formattedTimestamp = user.last_seen ? new Date(user.last_seen).toISOString() : null;
-  //     return {
-  //       ...user,
-  //       last_seen: formattedTimestamp,
-  //       isBlocked: user.isBlocked,
-  //     };
-  //   });
-  //   res.json(formattedUsers);
-  // });
-  const users = await User.findAll({
-  attributes: ["id", "name", "email", "last_seen", "isBlocked"]
-});
+  db.query("SELECT id, name, email, last_seen, isBlocked FROM users", (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Database error" });
+    }
+    const formattedUsers = results.map(user => {
+      const formattedTimestamp = user.last_seen ? new Date(user.last_seen).toISOString() : null;
+      return {
+        ...user,
+        last_seen: formattedTimestamp,
+        isBlocked: user.isBlocked,
+      };
+    });
+    res.json(formattedUsers);
+  });
 res.json(users);
-
 });
 
 app.post("/login", async (req, res) => {
@@ -95,25 +72,22 @@ app.post("/login", async (req, res) => {
   if (!email || !password) {
     return res.status(400).json({ message: "Email and Password required" });
   }
-  // db.query("SELECT * FROM users WHERE email = ?", [email], async (err, result) => {
-  //   if (err) return res.status(500).json({ error: err.message });
-  //   if (result.length === 0) {
-  //     return res.status(401).json({ message: "User not found" });
-  //   }
-  //   const user = result[0];
-  //   const isValidPassword = await bcrypt.compare(password, user.password);
-  //   if (!isValidPassword) {
-  //     return res.status(401).json({ message: "Invalid password" });
-  //   }
-  //    if (user.isBlocked) {
-  //     return res.status(403).json({ message: "Your account is blocked." });
-  //   }
-  //   db.query("UPDATE users SET last_seen = CURRENT_TIMESTAMP WHERE id = ?", [user.id]);
-  //   res.json({ message: "Login successful!", userId: user.id, name: user.name });
-  // });
-  const user = await User.findOne({ where: { email } });
-  await user.update({ last_seen: new Date() });
-
+  db.query("SELECT * FROM users WHERE email = ?", [email], async (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (result.length === 0) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    const user = result[0];
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+     if (user.isBlocked) {
+      return res.status(403).json({ message: "Your account is blocked." });
+    }
+    db.query("UPDATE users SET last_seen = CURRENT_TIMESTAMP WHERE id = ?", [user.id]);
+    res.json({ message: "Login successful!", userId: user.id, name: user.name });
+  });
 });
 
 app.delete("/delete-users", checkBlockedUser, async (req, res) => {
@@ -123,10 +97,8 @@ app.delete("/delete-users", checkBlockedUser, async (req, res) => {
     return res.status(400).json({ message: "No users selected for deletion" });
   }
   try {
-    // await db.promise().query("DELETE FROM users WHERE id IN (?)", [userIds]);
-    // res.json({ message: "Users deleted successfully" });
-    await User.destroy({ where: { id: userIds } });
-
+    await db.promise().query("DELETE FROM users WHERE id IN (?)", [userIds]);
+    res.json({ message: "Users deleted successfully" });
   } catch (error) {
     console.error("Error deleting users from database:", error);
     res.status(500).json({ message: "Internal server error" });
@@ -139,9 +111,8 @@ app.put("/block-users", checkBlockedUser, async (req, res) => {
     return res.status(400).json({ message: "No users selected for blocking" });
   }
   try {
-    // await db.promise().query("UPDATE users SET isBlocked = TRUE WHERE id IN (?)", [userIds]);
-    // res.json({ message: "Users blocked successfully" });
-    await User.update({ isBlocked: true }, { where: { id: userIds } });
+    await db.promise().query("UPDATE users SET isBlocked = TRUE WHERE id IN (?)", [userIds]);
+    res.json({ message: "Users blocked successfully" });
   } catch (error) {
     console.error("Error blocking users:", error);
     res.status(500).json({ message: "Internal server error" });
@@ -154,9 +125,8 @@ app.put("/unblock-users", checkBlockedUser, async (req, res) => {
     return res.status(400).json({ message: "No users selected for unblocking" });
   }
   try {
-    // await db.promise().query("UPDATE users SET isBlocked = FALSE WHERE id IN (?)", [userIds]);
-    // res.json({ message: "Users unblocked successfully" });
-    await User.update({ isBlocked: false }, { where: { id: userIds } });
+    await db.promise().query("UPDATE users SET isBlocked = FALSE WHERE id IN (?)", [userIds]);
+    res.json({ message: "Users unblocked successfully" });
   } catch (error) {
     console.error("Error unblocking users:", error);
     res.status(500).json({ message: "Internal server error" });
