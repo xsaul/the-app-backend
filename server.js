9deb793c34fc418d7827f472bf9484fc28fc4276
xsaul <saulglz0534@gmail.com>
2025-06-04 13:20:26 -0600

    changes env

--- server.js
+++ server.js
@@ -140,5 +140,5 @@ app.put("/unblock-users", checkBlockedUser, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
