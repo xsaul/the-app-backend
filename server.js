ff2d2cd852bfc301f13731907bce632b9f07774b
xsaul <saulglz0534@gmail.com>
2025-06-03 21:17:59 -0600

    changing server

--- server.js
+++ server.js
@@ -139,4 +139,5 @@ app.put("/unblock-users", checkBlockedUser, async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
