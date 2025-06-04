e1b4c0b649066bffe787118e21c1b62d54945c52
xsaul <saulglz0534@gmail.com>
2025-06-03 22:47:21 -0600

    MySQL connection settings

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: 3306 
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306 
 });
 db.connect((err) => {
   if (err) throw err;
