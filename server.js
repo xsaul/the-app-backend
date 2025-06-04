042373f9db7fc28d9633adb92b6c424437a79199
xsaul <saulglz0534@gmail.com>
2025-06-03 22:11:55 -0600

    Updated MySQL connection variables

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection(process.env.DB_URL);
-
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
 db.connect((err) => {
   if (err) throw err;
   console.log("Connected to MySQL");
