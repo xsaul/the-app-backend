0f70b3b2a08fddfc2585109c7421f03c1b81e01d
xsaul <saulglz0534@gmail.com>
2025-06-04 13:06:04 -0600

    changes env

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const db = mysql.createConnection({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306 
+  port: process.env.MYSQLPORT
 });
+
 db.connect((err) => {
   if (err) throw err;
   console.log("Connected to MySQL");
