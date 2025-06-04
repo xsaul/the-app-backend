c3a54a488b4168982e0f7e96908c478eef872c4d
xsaul <saulglz0534@gmail.com>
2025-06-03 22:44:06 -0600

    MySQL connection settings

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: 3306 
 });
 db.connect((err) => {
   if (err) throw err;
